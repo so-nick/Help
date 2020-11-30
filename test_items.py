fe666869be2e87ce94c0a4c086d5e9c0ad6bf031
So_nick <danilklimashov92@gmail.com>
2020-11-30 09:48:01 +0200

    lanuagetest

---
+++ test_items.py
@@ -0,0 +1,12 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_add_korzina(browser):
+    browser.get(link)
+    # Nahodim knopku kupit
+    korzina = browser.find_element_by_css_selector('button.btn:nth-child(3)').text
+    time.sleep(10)
+    assert 'Añadir al carrito' == korzina
+
+    time.sleep(5)
